32f37602885707ef289f28a66a113d420b486fbf
yaitalla <yaitalla.dev@gmail.com>
2018-12-27 16:56:16 +0100

    correc

--- client/src/components/headerCompo/index.js
+++ client/src/components/headerCompo/index.js
@@ -3,19 +3,13 @@ import { Link } from 'react-router-dom';
 import { flex, noBullet, btn} from './style';
 
 
-class Navbar extends React.Component {
-  constructor(props){
-    super(props);
-  }
 
-  render() {
-    return (
-      <div style={flex}>
-        <ul style={noBullet}>
-          <Link to={'/'}><button style={btn}> PLAY </button> </Link>
-        </ul>
-      </div>
-    )
-  }
-}
- export default Navbar;
+const Navbar = () => (
+  <div style={flex}>
+  <ul style={noBullet}>
+    <Link to={'/'}><button style={btn}> PLAY </button> </Link>
+  </ul>
+</div>
+);
+
+export default Navbar;
