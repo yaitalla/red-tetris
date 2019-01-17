8177897a670c3aa573a0ce788d4422f04fec5d3d
Lu <lubaujar@student.42.fr>
2019-01-17 17:03:07 +0100

    Update index.js

--- client/src/components/game/index.js
+++ client/src/components/game/index.js
@@ -8,6 +8,10 @@ import Launcher from './launcher';
 const { gridHeight, gridWidth, block } = constants;
 
 const Game = () =>
+// evite les style inline comme ca, fais une variable que tu importe de ton fihcier style.js
+// même si tu as seulement backgroundColor comme ici.
+// parce que sinon, en mode inline c'est recompiler a chaque fois,
+// alors que si c'est import d'un fichier ET si il n'y a pas de changement, il ne recharge pas ton style, il l'a déjà.
   <div  style={ {"backgroundColor": "#d0ddf2"}}>
 		<Stage width={gridWidth} height={gridHeight}>
 			<Layer>
